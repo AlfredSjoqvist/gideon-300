642687e312c4938a9deb0d45eac10bc53101bd71
Alfred Sjöqvist <alfred.sjoqvist@gmail.com>
2026-02-08 04:40:27 -0800

    Fix Supabase connection 2

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // 1. SCROLL UP on that page to find the "Project URL"
-const supabaseUrl = 'https://db.plvsuzgfgdtquaeazvqg.supabase.co' 
+const supabaseUrl = 'https://plvsuzgfgdtquaeazvqg.supabase.co' 
 const supabaseKey = 'sb_publishable_s6kvNOKumKhe0ER8Q1caVw_U2s_1ry3'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
